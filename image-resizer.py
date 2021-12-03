a76a5a88e55a5dae7e75d7b593ea9f983c8a80ec
The-Last-Cookie <koller.jonas@online.de>
2021-12-03 21:59:17 +0100

    add ability to resize specific files

--- image-resizer.py
+++ image-resizer.py
@@ -2,7 +2,8 @@ import os
 from PIL import Image
 
 PATH = r''
-MAX_SIZE = 800
+MAX_SIZE = 200
+FILES = []
 
 def resize_image(filename):
     filepath = PATH + '/' + filename
@@ -14,6 +15,11 @@ def resize_image(filename):
 
 def main():
     formats = ('.jpeg', '.jpg', '.png')
+
+    if FILES:
+        for file in FILES:
+            resize_image(file)
+        return
 
     for file in os.listdir(PATH):
         for format in formats:
