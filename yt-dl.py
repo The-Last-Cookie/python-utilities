b6a8193133ad8fefd8be6b4376744ca46e04b89f
The-Last-Cookie <koller.jonas@online.de>
2021-12-23 21:47:11 +0100

    add help message to script

--- yt-dl.py
+++ yt-dl.py
@@ -1,13 +1,31 @@
 import os
 import sys
 
-if len(sys.argv) < 2:
-    print('No YouTube link provided')
-    exit(0)
+def download_video():
+    # first argument is the file itself
+    if len(sys.argv) < 2:
+        print('No YouTube link provided')
+        filename = get_filename(sys.argv[0])
+        print('Usage: ' + filename + ' [YouTube link]')
+        return
 
-# first argument is the file itself
-link = sys.argv[1]
+    if (sys.argv[1] == 'help' or sys.argv[1] == '-h'):
+        filename = get_filename(sys.argv[0])
+        print('Usage: ' + filename + ' [YouTube link]')
+        return
 
-command = str('youtube-dl -o "%USERPROFILE%\Downloads\%(title)s.%(ext)s" ' + link + ' --extract-audio --audio-format mp3 --audio-quality 256K')
+    link = sys.argv[1]
 
-os.system(command)
+    command = str('youtube-dl -o "%USERPROFILE%\Downloads\%(title)s.%(ext)s" ' + link + ' --extract-audio --audio-format mp3 --audio-quality 256K')
+    os.system(command)
+
+def get_filename(path):
+    if path.rfind('\\') == -1:
+        filename = path
+    else:
+        last_occurence = path.rfind('\\')
+        filename = path[last_occurence + 1:]
+    
+    return filename
+
+download_video()
