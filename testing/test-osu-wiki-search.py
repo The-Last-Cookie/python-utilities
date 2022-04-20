584bb4e2aef5840a95bc3566f86d66f40e3d2c00
The-Last-Cookie <koller.jonas@online.de>
2022-04-20 14:45:48 +0200

    add new test cases

--- testing/test-osu-wiki-search.py
+++ testing/test-osu-wiki-search.py
@@ -1,8 +1,9 @@
-import unittest
+from unittest import TestCase
+from unittest import main as testing
 
 import search
 
-class TestGetRootLink(unittest.TestCase):
+class TestGetRootLink(TestCase):
     def test_return_whole_link(self):
         s = search.get_root_link('osu-wiki\\wiki', True)
         self.assertEqual(s, 'osu-wiki\\wiki\\')
@@ -35,5 +36,35 @@ class TestGetRootLink(unittest.TestCase):
         s = search.get_root_link('osu-wiki\\wiki\\S\\T\\Z', False)
         self.assertEqual(s, 'S\\T\\Z\\')
 
+class TestShouldBeExcluded(TestCase):
+    def test_should_not_exclude_on_empty_params(self):
+        b = search.should_be_excluded('', [], True)
+        self.assertFalse(b)
+        b = search.should_be_excluded('', [], False)
+        self.assertFalse(b)
+
+class TestFileTypeInFiles(TestCase):
+    def test_return_true_for_default_file_type(self):
+        b = search.file_type_in_files('.md', [])
+        self.assertTrue(b)
+        b = search.file_type_in_files('.md', ['en.md'])
+        self.assertTrue(b)
+        b = search.file_type_in_files('.md', ['en.md', 'de.md'])
+        self.assertTrue(b)
+
+    def test_return_true_if_file_type_in_files(self):
+        b = search.file_type_in_files('en.md', ['en.md'])
+        self.assertTrue(b)
+        b = search.file_type_in_files('en.md', ['en.md', 'de.md'])
+        self.assertTrue(b)
+        b = search.file_type_in_files('en.md', ['de.md', 'en.md'])
+        self.assertTrue(b)
+
+    def test_return_false_if_file_type_not_in_files(self):
+        b = search.file_type_in_files('en.md', [])
+        self.assertFalse(b)
+        b = search.file_type_in_files('en.md', ['de.md'])
+        self.assertFalse(b)
+
 if __name__ == '__main__':
-    unittest.main()
+    testing()
