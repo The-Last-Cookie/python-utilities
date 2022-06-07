ed4f3e6e43dfb1b80577a6268522706395c5bff6
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2022-06-07 15:43:59 +0200

    add converter for dates

---
+++ converters/date.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+import re
+
+def change_date_in_line(line: str, o_format: str, t_format: str) -> str:
+    iter = re.finditer(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', line)
+    matches = []
+
+    for m in iter:
+        match = line[m.start(0):m.end(0)]
+        matches.append(match)
+
+    for match in matches:
+        o_date = datetime.strptime(match, o_format)
+        t_date = o_date.strftime(t_format)
+        line = line.replace(o_date, t_date)
+
+    return line
+
+def change_date(file: str, o_format: str, t_format: str) -> None:
+    """
+    o_format: Original format that should be replaced
+    t_format: Format to convert to
+    """
+    lines = []
+    with open(file, mode='r', encoding='utf-8', newline='\n') as r:
+        lines = r.readlines()
+
+        for line_index, line in enumerate(lines):
+            new_line = change_date_in_line(line, o_format, t_format)
+            lines[line_index] = new_line
+
+    with open(file, mode='w', encoding='utf-8', newline='\n') as w:
+        w.writelines(lines)
+
+def main() -> None:
+    change_date('de.md', '%Y-%m-%d', '%d.%m.%Y')
+
+if __name__ == '__main__':
+    main()
