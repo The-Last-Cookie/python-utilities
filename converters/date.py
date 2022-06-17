5a5bca29f46f6b87f8ada33bf0b29f187c30b94e
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2022-06-17 18:32:54 +0200

    move parameters to inner function

--- converters/date.py
+++ converters/date.py
@@ -2,6 +2,10 @@ from datetime import datetime
 import re
 
 def change_date_in_line(line: str, o_format: str, t_format: str) -> str:
+    """
+    o_format: Original format that should be replaced
+    t_format: Format to convert to
+    """
     iter = re.finditer(r'[0-9]{4}-[0-9]{2}-[0-9]{2}', line)
     matches = []
 
@@ -16,24 +20,20 @@ def change_date_in_line(line: str, o_format: str, t_format: str) -> str:
 
     return line
 
-def change_date(file: str, o_format: str, t_format: str) -> None:
-    """
-    o_format: Original format that should be replaced
-    t_format: Format to convert to
-    """
+def change_date(file: str) -> None:
     lines = []
     with open(file, mode='r', encoding='utf-8', newline='\n') as r:
         lines = r.readlines()
 
         for line_index, line in enumerate(lines):
-            new_line = change_date_in_line(line, o_format, t_format)
+            new_line = change_date_in_line(line, '%Y-%m-%d', '%d.%m.%Y')
             lines[line_index] = new_line
 
     with open(file, mode='w', encoding='utf-8', newline='\n') as w:
         w.writelines(lines)
 
 def main() -> None:
-    change_date('de.md', '%Y-%m-%d', '%d.%m.%Y')
+    change_date('de.md')
 
 if __name__ == '__main__':
     main()
