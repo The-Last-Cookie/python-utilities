2eee80e9944e37bbf31af2d4e29f1ccb5612614d
The-Last-Cookie <50201659+The-Last-Cookie@users.noreply.github.com>
2024-09-23 00:36:56 +0200

    Add support for transparency

--- webp.py
+++ webp.py
@@ -11,15 +11,23 @@ def isAnimated(file_path: str) -> bool:
             return (ord(is_animated_byte) >> 1) & 1
         return False
 
+def isTransparent(img: Image) -> bool:
+    return img.mode == "RGBA" or "transparency" in img.info
+
 def convert(file_path: str, format = 'jpeg', overwrite = False, **params) -> None:
     new_file_path = file_path[:-4] + format
     img = Image.open(file_path)
+
+    # JPEG does not support alpha channel
+    if isTransparent(img) and format == 'jpeg':
+        img = img.convert('RGB')
+
     img.save(new_file_path, format, **params)
 
     if overwrite:
         os.remove(file_path)
 
-file_path = 'img.webp'
+file_path = ''
 if isAnimated(file_path):
     convert(file_path, 'gif', quality=95, optimize=True, save_all=True)
 else:
